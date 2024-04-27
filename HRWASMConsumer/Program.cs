5650bbb3a6f9b674474b3631ece46368b5a95d7c
Christen <christen.foad@gmail.com>
2024-04-27 13:38:58 +0300

    LAst Update

--- HRWASMConsumer/Program.cs
+++ HRWASMConsumer/Program.cs
@@ -13,14 +13,24 @@ namespace HRWASMConsumer
             builder.RootComponents.Add<App>("#app");
             builder.RootComponents.Add<HeadOutlet>("head::after");
 
-            builder.Services.AddScoped<IServices<Employee>, EmployeeService>();
+            //builder.Services.AddScoped<IServices<Employee>, EmployeeService>();
+           
+            //builder.Services.AddScoped<IServices<Department>, DepartmentService>();
+            //register HttpClient =API=>http://localhost:63289
+            //builder.Services.AddScoped(sp => 
+            //    new HttpClient {
+            //        BaseAddress = 
+            //            new Uri(builder.Configuration.GetValue<string>("IP"))
+            //    });
 
-            builder.Services.AddScoped(sp => 
-                new HttpClient {
-                    BaseAddress = 
-                        new Uri(builder.Configuration.GetValue<string>("IP"))
-                });
-
+            //http Ip
+            //httpclient - IService<Employee>
+            builder.Services.AddHttpClient<IServices<Employee>, EmployeeService>(
+                httpclient=>httpclient.BaseAddress=new Uri(builder.Configuration.GetValue<string>("IP"))
+                );
+            builder.Services.AddHttpClient<IServices<Department>, DepartmentService>(
+                httpclient=>httpclient.BaseAddress=new Uri(builder.Configuration.GetValue<string>("IP"))
+                );
             await builder.Build().RunAsync();
         }
     }
---
+++ HRWASMConsumer/Services/DepartmentService.cs
@@ -0,0 +1,39 @@
+﻿using HRWASMConsumer.Models;
+using System.Net.Http.Json;
+
+namespace HRWASMConsumer.Services
+{
+    public class DepartmentService : IServices<Department>
+    {
+        private readonly HttpClient httpClient;
+
+        public DepartmentService(HttpClient httpClient)
+        {
+            this.httpClient = httpClient;
+        }
+        public Task Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<List<Department>> GetAll()
+        {
+           return await httpClient.GetFromJsonAsync<List<Department>>("/api/Department");
+        }
+
+        public Task<Department> GetByID(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task Insert(Department item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task Update(int id, Department item)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
